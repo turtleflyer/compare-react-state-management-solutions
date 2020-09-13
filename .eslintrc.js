0290e3c4801dfe55a2ded62e7ff4bb10067274d4
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-09-13 01:26:54 -0700

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,32 @@
 const defRules = {
-  'comma-dangle': 'off',
-  'func-call-spacing': 'off',
-  'function-paren-newline': 'off',
-  'implicit-arrow-linebreak': 'off',
-  'import/no-extraneous-dependencies': 'off',
-  'linebreak-style': 'off',
-  'max-len': 'off',
-  'no-confusing-arrow': 'off',
   'no-nested-ternary': 'off',
   'no-plusplus': 'off',
-  'no-prototype-builtins': 'off',
-  'no-spaced-func': 'off',
-  'object-curly-newline': 'off',
-  'operator-linebreak': 'off',
   'import/extensions': 'off',
   'import/prefer-default-export': 'off',
-  indent: 'off',
   'jsx-a11y/label-has-associated-control': ['error', { assert: 'either', depth: 2 }],
-  'react/jsx-props-no-spreading': 'off',
-  'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.tsx'] }],
   'react/destructuring-assignment': 'off',
   'react/jsx-curly-newline': 'off',
+  'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.tsx'] }],
+  'react/jsx-props-no-spreading': 'off',
   'react/prop-types': 'off',
   'react/state-in-constructor': 'off',
 };
 
 module.exports = {
-  overrides: [
-    {
-      files: ['**/*.js', '**/*.jsx', '**/*.json'],
-      env: {
-        browser: true,
-        es2020: true,
-      },
-      extends: ['plugin:react/recommended', 'airbnb'],
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-      },
-      plugins: ['react'],
-      rules: { ...defRules },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb', 'prettier', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: { ...defRules },
+  overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
       settings: {
@@ -53,25 +36,15 @@ module.exports = {
           },
         },
       },
-      env: {
-        browser: true,
-        es2020: true,
-      },
-      extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended'],
+      extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
       parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-      },
-      plugins: ['react', '@typescript-eslint'],
       rules: {
         ...defRules,
         '@typescript-eslint/ban-types': ['error', { types: { object: false } }],
-        '@typescript-eslint/no-use-before-define': 'error',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': 'error',
         'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': 'error',
       },
     },
   ],
