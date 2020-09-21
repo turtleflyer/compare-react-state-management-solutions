6f28d895d51c4ca56eed6e7fb0634d5d1d5a26dc
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-09-20 21:50:19 -0700

    Fix same random pixel repainted back to back

--- src/controls/RandomPaintButton.tsx
+++ src/controls/RandomPaintButton.tsx
@@ -8,18 +8,18 @@ import { storeAtomsMethods } from '../State/storeAtomsMethods';
 export const RandomPaintButton: FC = () => {
   const gridSize = useRecoilValue(gridSizeState);
 
-  const [randomIndexToPaint, setIndex] = useState(placeholderAtomForPixelControl);
-  const paintRandomPixel = useSetRecoilState(randomIndexToPaint);
+  const [randomAtomToPaint, setIndex] = useState([placeholderAtomForPixelControl]);
+  const paintRandomPixel = useSetRecoilState(randomAtomToPaint[0]);
 
   useEffect(() => {
-    if (randomIndexToPaint) {
+    if (randomAtomToPaint) {
       paintRandomPixel((prev) => (1 - prev) as 0 | 1);
     }
-  }, [paintRandomPixel, randomIndexToPaint]);
+  }, [paintRandomPixel, randomAtomToPaint]);
 
   function randomPaint() {
     const randomIndex = Math.floor(Math.random() * gridSize ** 2);
-    setIndex(storeAtomsMethods.get(randomIndex));
+    setIndex([storeAtomsMethods.get(randomIndex)]);
   }
 
   return (
