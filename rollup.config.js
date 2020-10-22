e47186dcfe9a61eee8f6d0c7be390f9ab8328f60
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-21 21:01:19 -0700

    Add declaration emmited

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+const outDir = 'local_packages/use-interstate-component';
+
 export default {
   input: 'src/App.tsx',
   output: {
-    dir: 'use-interstate-component',
+    dir: outDir,
     format: 'es',
   },
   external: ['react', 'use-measure-perf'],
