7340e3eb60cb39314839c0eebfbf1dd211a00ef4
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-21 23:15:46 -0700

    Add both implementation side by side

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import type { FC } from 'react';
 import React from 'react';
+import { App as RecoilApp } from 'recoil-component';
+import { App as UseInterstateApp } from 'use-interstate-component';
 
 export const App: FC = () => {
-  return <div></div>;
+  return (
+    <div {...{ style: { display: 'flex' } }}>
+      <div>
+        <RecoilApp />
+      </div>
+      <div>
+        <UseInterstateApp />
+      </div>
+    </div>
+  );
 };
