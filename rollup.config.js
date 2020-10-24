7bec546fb405135b18d285823c75fbc41629191b
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-23 23:14:15 -0700

    Update project accordingly to npm 7 and react 17

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const outDir = 'local_packages/recoil-component';
+const outDir = 'compiled_bundles/recoil';
 
 export default {
   input: 'src/App.tsx',
