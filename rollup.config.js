d1785b90d90f9b5f67d79bb4a00580fa3e4b406e
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-23 19:45:06 -0700

    Update project accordingly to npm 7 and react 17

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const outDir = 'local_packages/use-interstate-component';
+const outDir = 'compiled_bundles/use-interstate';
 
 export default {
   input: 'src/App.tsx',
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { measure } from 'use-measure-perf';
 import { App } from './App.tsx';
 import './index.css';
-import { measure } from './perf-measure/useMeasurePerformance.ts';
 
 measure('start');
 
