b28636e383a01e4656a96cbaf6ee935a1b4c1773
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-24 02:18:07 -0700

    Update project accordingly to npm 7 and react 17

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { measure } from 'use-measure-perf';
 import { App } from './App.tsx';
 import './index.css';
+
+measure('start');
 
 ReactDOM.render(
   <React.StrictMode>
