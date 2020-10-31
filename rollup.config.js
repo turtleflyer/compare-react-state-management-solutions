1c20abf92f15f9ec8fa6c4ffbfa15b3f9d35b415
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-31 01:46:08 -0700

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+process.env.BABEL_ENV = 'production';
 const outDir = 'compiled_bundles/recoil';
 
 export default {
@@ -10,6 +12,11 @@ export default {
     dir: outDir,
     format: 'es',
   },
-  external: ['react', 'use-measure-perf'],
-  plugins: [typescript(), nodeResolve(), commonjs()],
+  external: ['react', 'react-dom', 'use-measure-perf', /^@babel\/runtime/],
+  plugins: [
+    babel({ babelHelpers: 'runtime', presets: ['react-app'] }),
+    typescript({ target: 'es6' }),
+    nodeResolve(),
+    commonjs(),
+  ],
 };
