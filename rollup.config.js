a1c546d42b0365cd28cd9ce9ff6583edf4506d8c
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-10-30 20:15:07 -0700

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+
+process.env.BABEL_ENV = 'production';
 
 const outDir = 'compiled_bundles/use-interstate';
 
@@ -10,6 +12,10 @@ export default {
     dir: outDir,
     format: 'es',
   },
-  external: ['react', 'use-measure-perf'],
-  plugins: [typescript(), nodeResolve(), commonjs()],
+  external: ['react', 'use-measure-perf', /^@babel\/runtime/],
+  plugins: [
+    babel({ babelHelpers: 'runtime', presets: ['react-app'] }),
+    typescript({ target: 'es6' }),
+    nodeResolve(),
+  ],
 };
