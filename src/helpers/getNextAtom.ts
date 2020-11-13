71de72ef3947b9d726c69c516221496cdaba9d72
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-11-13 03:10:12 -0800

    Implement use-perf-observer

--- src/helpers/getNextAtom.ts
+++ src/helpers/getNextAtom.ts
@@ -1,17 +1,6 @@
+import { getNextKey } from 'get-next-key';
 import { atom, RecoilState } from 'recoil';
 import type { State } from '../State/StateInterface';
-
-const prefCountsRecords = new Map<string, number>();
-export function getNextKey<K extends string>(prefix: K): K {
-  let count = 0;
-  if (prefCountsRecords.has(prefix)) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    count = prefCountsRecords.get(prefix)! + 1;
-  }
-  prefCountsRecords.set(prefix, count);
-
-  return `${prefix}-${count}` as K;
-}
 
 export function getNextAtom<K extends keyof State, D extends State[K] = State[K]>(
   prefix: K,
