0d415c832bd6c8da046d028ea76b6b9fa73d6552
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-11-13 20:17:38 -0800

    Restructure code using new inner dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { measure } from 'use-measure-perf';
 import { App } from './App.tsx';
 import './index.css';
-
-measure('start');
 
 ReactDOM.render(<App />, document.getElementById('root'));
