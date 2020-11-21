3ca966e1ea42253d471c97ed710f34c1c4a34cd5
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-11-20 17:04:53 -0800

    Add forwarded ref to Button component

--- src/reusable-components/Button.tsx
+++ src/reusable-components/Button.tsx
@@ -1,14 +1,20 @@
-import type { CSSProperties, FC } from 'react';
-import React from 'react';
+import type { CSSProperties, RefObject } from 'react';
+import React, { forwardRef } from 'react';
 
 const style: CSSProperties = { width: 200, height: 20, display: 'block', marginRight: 5 };
 
-export const Button: FC<{ callback: () => void; addStyle?: CSSProperties; name?: string }> = ({
-  callback,
-  addStyle = {},
-  name = 'start',
-}) => {
+export const Button = forwardRef<
+  HTMLElement,
+  { callback: () => void; addStyle?: CSSProperties; name?: string }
+>(function Button({ callback, addStyle = {}, name = 'start' }, ref) {
   const calcStyle = { ...style, ...addStyle };
 
-  return <button {...{ style: calcStyle, type: 'button', onClick: callback }}>{name}</button>;
-};
+  return (
+    <button
+      {...{ style: calcStyle, type: 'button', onClick: callback }}
+      {...({ ref } as { ref: RefObject<HTMLButtonElement> })}
+    >
+      {name}
+    </button>
+  );
+});
