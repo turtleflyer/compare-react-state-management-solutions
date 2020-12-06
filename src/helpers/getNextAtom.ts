9cdbff40c261bf3a4d8a025e53db2b13b65cc312
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-12-05 17:06:17 -0800

    Simplify getNextAtom

--- src/helpers/getNextAtom.ts
+++ src/helpers/getNextAtom.ts
@@ -1,9 +1,6 @@
 import { getNextKey } from 'get-next-key';
 import type { Atom, State } from '../State/StateInterface';
 
-export function getNextAtom<K extends keyof State, D extends State[K] = State[K]>(
-  prefix: K,
-  defaultValue: D
-): Atom<K> {
+export function getNextAtom<K extends keyof State>(prefix: K, defaultValue: State[K]): Atom<K> {
   return [getNextKey(prefix), defaultValue];
 }
