981524ccc81d7baf8cf6d13cc7b695500cc47c1b
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-12-08 18:22:39 -0800

    Set default grid size to 32

--- src/State/State.ts
+++ src/State/State.ts
@@ -21,7 +21,7 @@ import {
   rememberActiveChoiceKey,
 } from './StateInterface';
 
-export const DEF_GRID_SIZE = 1;
+export const DEF_GRID_SIZE = 32;
 export const DEF_COLOR = '#AAAAAA';
 export const INPUT_WAITING_DELAY = 3000;
 export const DEF_PIXELS_PERCENT_TO_PAINT = 30;
