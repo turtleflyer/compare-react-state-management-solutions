a712ed7ee4e350bcca27fed0b1f6a5f34f5b4cff
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-12-13 13:05:08 -0800

    Add redux-hooks-component, update version other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import type { FC } from 'react';
+import type { CSSProperties, FC } from 'react';
 import React from 'react';
 import { App as RecoilApp } from 'recoil-component';
 import { App as ReduxConnectApp } from 'redux-connect-component';
+import { App as ReduxHooksApp } from 'redux-hooks-component';
 import { App as UseInterstateApp } from 'use-interstate-component';
 
+const appsContainerStyle: CSSProperties = { display: 'flex' };
+
 export const App: FC = () => (
-  <div {...{ style: { display: 'flex' } }}>
-    <div>
-      <RecoilApp />
-    </div>
-    <div>
-      <UseInterstateApp />
-    </div>
-    <div>
-      <ReduxConnectApp />
+  <div {...{ style: { display: 'flex', flexDirection: 'column', height: 'calc(100vh - 20px)' } }}>
+    <div {...{ style: { display: 'flex', flexGrow: 1 } }}>
+      <div {...{ style: appsContainerStyle }}>
+        <RecoilApp />
+      </div>
+      <div {...{ style: appsContainerStyle }}>
+        <UseInterstateApp />
+      </div>
+      <div {...{ style: appsContainerStyle }}>
+        <ReduxConnectApp />
+      </div>
+      <div {...{ style: appsContainerStyle }}>
+        <ReduxHooksApp />
+      </div>
     </div>
   </div>
 );
