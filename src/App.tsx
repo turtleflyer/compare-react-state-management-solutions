33d66d973a90241234f45bafb7b47ec6af308ebc
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-12-14 20:20:13 -0800

    Rename internal packages using unified prefix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { App as RecoilApp } from '@~internal/recoil-component';
+import { App as ReduxConnectApp } from '@~internal/redux-connect-component';
+import { App as ReduxHooksApp } from '@~internal/redux-hooks-component';
+import { App as UseInterstateApp } from '@~internal/use-interstate-component';
 import type { CSSProperties, FC } from 'react';
 import React from 'react';
-import { App as RecoilApp } from 'recoil-component';
-import { App as ReduxConnectApp } from 'redux-connect-component';
-import { App as ReduxHooksApp } from 'redux-hooks-component';
-import { App as UseInterstateApp } from 'use-interstate-component';
 
 const appsContainerStyle: CSSProperties = { display: 'flex' };
 
 export const App: FC = () => (
   <div {...{ style: { display: 'flex', flexDirection: 'column', height: 'calc(100vh - 20px)' } }}>
-    <div {...{ style: { margin: '5px 0 0 10px', fontWeight: 'bold' } }}>v.1.0.0</div>
+    <div {...{ style: { margin: '5px 0 0 10px', fontWeight: 'bold' } }}>v.1.1.0</div>
     <div {...{ style: { display: 'flex', flexGrow: 1 } }}>
       <div {...{ style: appsContainerStyle }}>
         <RecoilApp />
