0793744a74273cbc68c7e6c2a1b8c38b08d09a4f
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2020-12-17 15:02:45 -0800

    Add error information to usePerfObserver and popup tips to PerformanceInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TipsPoolProvider } from '@~internal/performance-info';
 import { App as RecoilApp } from '@~internal/recoil-component';
 import { App as ReduxConnectApp } from '@~internal/redux-connect-component';
 import { App as ReduxHooksApp } from '@~internal/redux-hooks-component';
@@ -8,21 +9,23 @@ import React from 'react';
 const appsContainerStyle: CSSProperties = { display: 'flex' };
 
 export const App: FC = () => (
-  <div {...{ style: { display: 'flex', flexDirection: 'column', height: 'calc(100vh - 20px)' } }}>
-    <div {...{ style: { margin: '5px 0 0 10px', fontWeight: 'bold' } }}>v.1.2.0</div>
-    <div {...{ style: { display: 'flex', flexGrow: 1 } }}>
-      <div {...{ style: appsContainerStyle }}>
-        <RecoilApp />
-      </div>
-      <div {...{ style: appsContainerStyle }}>
-        <UseInterstateApp />
-      </div>
-      <div {...{ style: appsContainerStyle }}>
-        <ReduxConnectApp />
-      </div>
-      <div {...{ style: appsContainerStyle }}>
-        <ReduxHooksApp />
+  <TipsPoolProvider>
+    <div {...{ style: { display: 'flex', flexDirection: 'column', height: 'calc(100vh - 20px)' } }}>
+      <div {...{ style: { margin: '5px 0 0 10px', fontWeight: 'bold' } }}>v.1.3.0</div>
+      <div {...{ style: { display: 'flex', flexGrow: 1 } }}>
+        <div {...{ style: appsContainerStyle }}>
+          <RecoilApp />
+        </div>
+        <div {...{ style: appsContainerStyle }}>
+          <UseInterstateApp />
+        </div>
+        <div {...{ style: appsContainerStyle }}>
+          <ReduxConnectApp />
+        </div>
+        <div {...{ style: appsContainerStyle }}>
+          <ReduxHooksApp />
+        </div>
       </div>
     </div>
-  </div>
+  </TipsPoolProvider>
 );
--- src/internal_packages/use-perf-observer/MeasureComponent.ts
+++ src/internal_packages/use-perf-observer/MeasureComponent.ts
@@ -29,12 +29,16 @@ export const MeasureComponent: FC<
   const { measureFromCreating, name } = settings;
 
   const [perfMarkName] = useState(() => getNextKey(name ?? 'start-use-perf-metrics'));
-  const [childrenProps, setChildrenProps] = useState<Required<MetricsComponentProps>>(() => {
+  const [childrenProps, setChildrenProps] = useState<MetricsComponentProps>(() => {
     if (isSupported) {
       return { data: null, status: 'never' };
     }
 
-    return { data: null, status: 'error' };
+    return {
+      data: null,
+      status: 'error',
+      error: Error('(usePerfObserver) The browser does not support the library'),
+    };
   });
   const firstTimeRunRec = useRef(true);
   const [conditionalObserverResult] = useState(() =>
@@ -57,7 +61,7 @@ export const MeasureComponent: FC<
       firstTimeRunRec.current = false;
 
       if (measureFromCreating) {
-        setChildrenProps((info) => ({ ...info, status: 'pending' }));
+        setChildrenProps({ status: 'pending', data: null });
       }
 
       return callback();
