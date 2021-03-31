516969fd4ca7cae6a73f8a0c261ae390e666048f
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2021-03-31 13:24:22 -0700

    Change eslint and perttier settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
   printWidth: 100,
+  endOfLine: 'lf',
+  semi: true,
+  arrowParens: 'always',
+  parser: 'typescript',
 };
