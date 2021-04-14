101853da49db3cb3afe14ad71142ca734284d124
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2021-04-13 18:07:37 -0700

    Delete unused file

--- src/helpers/getNextAtom.ts
+++
@@ -1,7 +0,0 @@
-// import { getNextKey } from 'get-next-key';
-// import type { Atom, State } from '../State/StateInterface';
-
-// export function getNextAtom<K extends keyof State>(prefix: K, defaultValue: State[K]): Atom<K> {
-//   return [getNextKey(prefix), defaultValue];
-// }
-export {};
