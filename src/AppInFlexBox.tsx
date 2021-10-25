3e5ad52b5bcc4336331b3f28b0e9de12093b92c8
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2021-10-24 23:05:23 -0700

    Add the link to the description of the project

--- src/AppInFlexBox.tsx
+++ src/AppInFlexBox.tsx
@@ -6,7 +6,7 @@ import { App as RecoilApp } from '@compare-react-state-management-solutions/reco
 import { App as ReduxHooksApp } from '@compare-react-state-management-solutions/redux-hooks-component';
 import { App as UseInterstateApp } from '@compare-react-state-management-solutions/use-interstate-component';
 import type { CSSProperties, FC } from 'react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { version } from '../package.json';
 import { InfoPanel, TAB_HEADER_HEIGHT } from './internal_packages/info-panel/InfoPanel';
 
@@ -25,6 +25,12 @@ const mainContainerStyle: CSSProperties = {
 };
 
 const versionInfoStyle: CSSProperties = { margin: '5px 0 0 10px', fontWeight: 'bold' };
+
+const readmeStyle: CSSProperties = {
+  display: 'inline-block',
+  fontWeight: 'normal',
+  marginLeft: '0.5em',
+};
 
 const appContainerStyle: CSSProperties = {
   display: 'flex',
@@ -54,7 +60,17 @@ export const AppInFlexBox: FC = () => {
             style: { ...mainContainerStyle, height: viewportHeight },
           }}
         >
-          <div {...{ style: versionInfoStyle }}>{`v.${version}`}</div>
+          <div {...{ style: versionInfoStyle }}>
+            {`v.${version} •`}
+            <a
+              {...{
+                href: 'https://github.com/turtleflyer/compare-react-state-management-solutions#readme',
+                style: readmeStyle,
+              }}
+            >
+              Learn about this project
+            </a>
+          </div>
 
           <div {...{ style: appContainerStyle }}>
             <ReduxHooksApp {...{ defGridSize: DEF_GRID_SIZE }} />
